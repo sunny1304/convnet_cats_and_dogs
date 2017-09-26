ecb37feca0a245befcf412eeab4d5927a092d363
farhan tanvir <sunny1304.ad1@gmail.com>
2017-09-26 23:23:58 +0600

    Create cats_and_dogs.py

---
+++ cats_and_dogs.py
@@ -0,0 +1,44 @@
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+
+train_dir='/Users/YourUserName/Documents/cats_and_dogs_small/train'
+validation_dir= '/Users/YourUserName/Documents/cats_and_dogs_small/validation'
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+
+
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D(2,2))
+
+model.add(layers.Conv2D(62, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
+
+model.add(layers.Conv2D(128, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
+
+model.add(layers.Conv2D(128, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
+
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+model.compile(loss='binary_crossentropy', optimizer= optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+model.fit_generator(train_generator, steps_per_epoch=100, epochs=30, validation_data=validation_generator, validation_steps=50)
+
+model.save('cats_and_dogs_small_1.h5')
+
+
+
